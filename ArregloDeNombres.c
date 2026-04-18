4d9fabb1fcb42c34eddc9ac66893e9c9848bf0f2
Javier Muraki <javimuraki@gmail.com>
2026-04-18 16:19:05 -0300

    Funcion de busqueda de nombres

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -7,12 +7,15 @@
 // Recibe el vector para mostrar todos los nombres alamcenados
 void MostrarPersonas(char *Vector[]);
 
+// Busca el nombre mediante ID
+void BuscarNombre(char *Vector[], int id);
+
 int main() {
     char *Buff = (char *) malloc(sizeof(char) * 128); // Variable auxiliar
     char *V[CantNombres];
     int i;
 
-    printf("Ingrese %d nombres:\n", CantNombres);
+    printf("\nIngrese %d nombres:\n", CantNombres);
     for (i = 0; i < CantNombres; i++) {
         gets(Buff);
         V[i] = (char *) malloc(sizeof(char) * (strlen(Buff) + 1));
@@ -21,6 +24,15 @@ int main() {
 
     // Ej 2 / Parte 1 / Apartado 2
     MostrarPersonas(V);
+
+    // Ej 2 / Parte 2 / Apartado 1
+    int id;
+
+    fflush(stdin); // Limpia el buffer del input de la consola
+    printf("\nIngrese una ID para buscar (0 - %d): ", CantNombres - 1);
+    scanf("%d", &id);
+
+    BuscarNombre(V, id);
 
     // Liberar memoria
     free(Buff);
@@ -36,6 +48,14 @@ void MostrarPersonas(char *Vector[]) {
 
     printf("\nNombres almacenados:\n");
     for (i = 0; i < CantNombres; i++) {
-        printf("%d - %s\n", i + 1, Vector[i]);
+        printf("%d - %s\n", i, Vector[i]);
+    }
+}
+
+void BuscarNombre(char *Vector[], int id) {
+    if (id >= 0 && id <= CantNombres - 1) {
+        printf("Nombre encontrado: %s\n", Vector[id]);
+    } else {
+        printf("No se encontro el valor buscado\n");
     }
 }
