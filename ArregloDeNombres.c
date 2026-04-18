96dd22b008aef257a17f63084a332fc5469f74c4
Javier Muraki <javimuraki@gmail.com>
2026-04-18 15:35:05 -0300

    Listado de nombres

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -2,23 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define CantNombres 5
+
+// Recibe el vector para mostrar todos los nombres alamcenados
+void MostrarPersonas(char *Vector[]);
+
 int main() {
     char *Buff = (char *) malloc(sizeof(char) * 128); // Variable auxiliar
-    char *V[5];
+    char *V[CantNombres];
     int i;
 
-    printf("Ingrese 5 nombres:\n");
-    for (i = 0; i < 5; i++) {
+    printf("Ingrese %d nombres:\n", CantNombres);
+    for (i = 0; i < CantNombres; i++) {
         gets(Buff);
         V[i] = (char *) malloc(sizeof(char) * (strlen(Buff) + 1));
         strcpy(V[i], Buff);
     }
 
+    // Ej 2 / Parte 1 / Apartado 2
+    MostrarPersonas(V);
+
     // Liberar memoria
     free(Buff);
-    for (i = 0; i < 5; i++) {
+    for (i = 0; i < CantNombres; i++) {
         free(V[i]);
     }
 
     return 0;
+}
+
+void MostrarPersonas(char *Vector[]) {
+    int i;
+
+    printf("\nNombres almacenados:\n");
+    for (i = 0; i < CantNombres; i++) {
+        printf("%d - %s\n", i + 1, Vector[i]);
+    }
 }
